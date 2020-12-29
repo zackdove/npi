3387437650137e9488336f39fbd9b19a5b92afa9
Zack Dove <zackeray.dove@ibm.com>
2020-12-29 14:50:44 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      current_view: 0,
+    }
+  }
+  handleClick(){
+    alert("hello");
+  }
+  render(){
+    return (
+      <Front onClick={ () => this.handleClick() }/>
+    );
+  }
+
+}
+
+class Front extends React.Component {
+  render() {
+    return (
+      <div className="page">
+        <h1>
+          narcissism
+        </h1>
+        <button onClick={ () => this.props.onClick() }>
+          begin test
+        </button>
+      </div>
+    );
+  }
+}
+
+class Questions extends React.Component{
+  render() {
+    return (
+      <div>
+        Questions go here
+      </div>
+    );
+  }
+}
+
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Main />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
